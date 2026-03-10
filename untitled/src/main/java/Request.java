6ff0551713d434932f147318a47acfd366958a7a
denfranke <33222627+denfranke@users.noreply.github.com>
2026-03-11 01:13:03 +0300

    -Https homework

---
+++ untitled/src/main/java/Request.java
@@ -0,0 +1,45 @@
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URIBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class Request {
+    private final String method;
+    private final String path;
+    private final List<NameValuePair> queryParams;
+
+    public Request(String method, String path) throws URISyntaxException {
+        this.method = method;
+        this.path = path;
+        this.queryParams = parseQueryParams();
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getQueryParam(String name) {
+        return queryParams.stream()
+                .filter(x -> x.getName().equals(name))
+                .findFirst()
+                .map(NameValuePair::getValue)
+                .orElse(null);
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    private List<NameValuePair> parseQueryParams() throws URISyntaxException {
+        URIBuilder uriBuilder = new URIBuilder(path);
+        return uriBuilder.getQueryParams();
+    }
+
+
+}
